a3e3c22b33aa26e2a706fa0ef76873396a4b1e62
Shindze <vainglory.ru@mail.ru>
2022-10-09 16:09:42 +0300

    UX: add autolayout and navigation

--- app/src/main/java/ru/mirea/whowantstobeamillionaire/view/MainActivity.java
+++ app/src/main/java/ru/mirea/whowantstobeamillionaire/view/MainActivity.java
@@ -1,18 +1,26 @@
-package ru.mirea.whowantstobeamillionaire;
+package ru.mirea.whowantstobeamillionaire.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import android.os.Bundle;
 import android.view.View;
 
+import ru.mirea.whowantstobeamillionaire.R;
+
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR);
 
+        NavController navHostController;
+        navHostController = Navigation.findNavController(this, R.id.nav_host);
     }
 }
--- app/src/main/java/ru/mirea/whowantstobeamillionaire/view/StartFragment.java
+++ app/src/main/java/ru/mirea/whowantstobeamillionaire/view/StartFragment.java
@@ -3,19 +3,33 @@ package ru.mirea.whowantstobeamillionaire.view;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+
+import java.util.Objects;
 
 import ru.mirea.whowantstobeamillionaire.R;
 
 public class StartFragment extends Fragment {
 
+    Button next_button;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         return inflater.inflate(R.layout.fragment_start, container, false);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        next_button = (Button) getView().findViewById(R.id.next_button);
+        next_button.setOnClickListener(v -> Navigation.findNavController(getView()).navigate(R.id.action_startFragment_to_mainFragment));
     }
 }
