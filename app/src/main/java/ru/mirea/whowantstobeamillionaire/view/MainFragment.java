7d36e281dc81b9e794418286b72bccb19cf6c81c
Shindze <vainglory.ru@mail.ru>
2022-10-10 19:47:24 +0300

    UX: add failed dialog

--- app/src/main/java/ru/mirea/whowantstobeamillionaire/view/MainFragment.java
+++ app/src/main/java/ru/mirea/whowantstobeamillionaire/view/MainFragment.java
@@ -1,5 +1,6 @@
 package ru.mirea.whowantstobeamillionaire.view;
 
+import android.app.NotificationManager;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AlertDialog;
@@ -13,56 +14,61 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+
+import org.junit.Assert;
+import org.junit.Test;
+
 import java.util.List;
 import java.util.Random;
-
-
-
-import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 
 import ru.mirea.whowantstobeamillionaire.R;
 
 public class MainFragment extends Fragment {
 
     List<Question> questions = new QuestionsDataBase().getQuestion();
-    Button answerA;
-    Button answerB;
-    Button answerC;
-    Button answerD;
+    Button answerA, answerB, answerC, answerD, positive_button, negative_button, answer_a_button;
     TextView questionText;
+
+    public NotificationManager notificationManager;
+    private  static final int NOTIFY_ID = 1;
+    private static final String CHANNEL_ID = "CHANNEL_ID";
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_main, container, false);
+
         questionText = view.findViewById(R.id.question_text);
+
         answerA = view.findViewById(R.id.answer_a_button);
         answerB = view.findViewById(R.id.answer_b_button);
         answerC = view.findViewById(R.id.answer_g_button2);
         answerD = view.findViewById(R.id.answer_v_button);
         loadQuestion(1);
+
         return view;
     }
 
-//    @Test
-//    public void getQuestionTest(){
-//        String expectedAnswer = "rightAnswer";
-//        String realAnswer = "";
-//        Assert.assertEquals(expectedAnswer, realAnswer);
-//    }
-//
-//    @Test
-//    private void getFailedDialog() {
-//        Boolean isFail = false;
-//        Boolean getFail = null;
-//        Assert.assertEquals(isFail, getFail);
-//    }
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        answer_a_button = (Button) getView().findViewById(R.id.answer_a_button);
+        answer_a_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                failDialog();
+            }
+        });
+
+    }
 
     void loadQuestion(int index){
         Question question = questions.get(index);
         questionText.setText(question.getQuestion());
         List<String> answers = question.getWrongAnswers();
-        answers.add(question.getRightAnswer());
         int randIndex = new Random().nextInt(4);
         answerA.setText(answers.get(randIndex));
         answers.remove(randIndex);
@@ -74,5 +80,49 @@ public class MainFragment extends Fragment {
         answers.remove(randIndex);
         answerD.setText(answers.get(0));
     }
+
+    private void failDialog() {
+        MaterialAlertDialogBuilder builder = new MaterialAlertDialogBuilder(getContext());
+        builder.setBackground(getResources().getDrawable(R.drawable.dialog_shape));
+        ConstraintLayout cl = (ConstraintLayout) getLayoutInflater().inflate(R.layout.fail_dialog, null);
+
+        builder.setCancelable(false);
+        builder.setView(cl);
+        AlertDialog dialog = builder.show();
+
+        positive_button = (Button) cl.findViewById(R.id.positive_button);
+        negative_button = (Button) cl.findViewById(R.id.netagive_button);
+
+        positive_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Начинаем заново)", Toast.LENGTH_LONG).show();
+                dialog.dismiss();
+            }
+        });
+
+        negative_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                getActivity().finish();
+            }
+        });
+
+        
+    }
+
+    //    @Test
+//    public void getQuestionTest(){
+//        String expectedAnswer = "rightAnswer";
+//        String realAnswer = "";
+//        Assert.assertEquals(expectedAnswer, realAnswer);
+//    }
+//
+//    @Test
+//    private void getFailedDialog() {
+//        Boolean isFail = false;
+//        Boolean getFail = true;
+//        Assert.assertEquals(isFail, getFail);
+//    }
 
 }
